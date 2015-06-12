2376e79a28626161d57a1abbf022f3c874480121
Andrew McWilliams <andy@jahya.net>
2015-06-12 17:05:22 -0400

    Sets up example code for audio unit use

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,61 +1,110 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
 void ofApp::setup(){
+    playing = false;
+    note = 60;
 
+    //Audio Unit Manager stuff
+    manager.setup();
+    manager.toggleDebugUI();
+
+    //Chain 1
+    manager.add(&chain1, "tal-one", ofColor::blue);
+
+    chain1.link(&noiseMaker1)
+          .to(&filter1)
+          .to(&reverb1)
+          .toMixer();
+
+    manager.loadPresets(&chain1);
+    
+    //Chain 2
+    manager.add(&chain2, "tal-two", ofColor::red);
+
+    chain2.link(&noiseMaker2)
+          .to(&filter2)
+          .to(&reverb2)
+          .toMixer();
+
+    manager.loadPresets(&chain2);
+
+    //ofxBpm
+    ofAddListener(bpm.beatEvent, this, &ofApp::play);
+    bpm.start();
 }
 
-//--------------------------------------------------------------
+void ofApp::play(void){
+    if(playing) {
+        chain1.midi()->sendNoteOn(1, note);
+        chain2.midi()->sendNoteOn(1, note);
+    }
+}
+
+void ofApp::togglePlaying() {
+    playing = !playing;
+    if(!playing) {
+        chain1.midi()->sendNoteOff(1, note);
+        chain2.midi()->sendNoteOff(1, note);
+    }
+}
+
 void ofApp::update(){
-
+    /*float cutoff = ofMap(sin(ofGetFrameNum() * 0.05), -1, 1, 0, 1);
+    noiseMaker1.set(TALNoiseMaker_cutoff, cutoff);*/
 }
 
-//--------------------------------------------------------------
 void ofApp::draw(){
-
+    manager.draw();
 }
 
-//--------------------------------------------------------------
+void ofApp::exit() {
+    manager.exit();
+}
+
 void ofApp::keyPressed(int key){
-
+    if(key == ' ') {
+        togglePlaying();
+    } else if(key == '[') {
+        togglePlaying();
+        note--;
+        togglePlaying();
+    } else if(key == ']') {
+        togglePlaying();
+        note++;
+        togglePlaying();
+    } else {
+        manager.keyPressed(key);
+    }
 }
 
-//--------------------------------------------------------------
 void ofApp::keyReleased(int key){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
 
 }
 
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,22 +1,40 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxAudioUnit.h"
+#include "ofxAudioUnitManager.h"
+#include "ofxBpm.h"
+#include "TALNoiseMaker.h"
+#include "LowPassFilter.h"
+#include "Reverb.h"
 
 class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void play();
+    void togglePlaying();
+    void exit();
 
-	public:
-		void setup();
-		void update();
-		void draw();
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    ofxAudioUnitManager manager;
+    AudioUnitChain chain1, chain2;
+    TALNoiseMaker noiseMaker1, noiseMaker2;
+    LowPassFilter filter1, filter2, filter3;
+    Reverb reverb1, reverb2;
+
+    ofxBpm bpm;
+    bool playing;
+    int note;
 };
